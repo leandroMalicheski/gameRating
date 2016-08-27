e360d7c1523fae559d711aff453ba47fe7b85f0e
Leandro <leandro.malicheski@gmail.com>
2016-08-27 19:40:19 -0300

    Inicio

---
+++ GameRating/platforms/android/assets/www/js/routes.js
@@ -0,0 +1,245 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.home', {
+    url: '/home',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('menu.buscarUsuRio', {
+    url: '/buscarUsuario',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/buscarUsuRio.html',
+        controller: 'buscarUsuRioCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu',
+    templateUrl: 'templates/menu.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('cadastro', {
+    url: '/cadastro',
+    templateUrl: 'templates/cadastro.html',
+    controller: 'cadastroCtrl'
+  })
+
+  .state('menu.perfil', {
+    url: '/perfil',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/perfil.html',
+        controller: 'perfilCtrl'
+      }
+    }
+  })
+
+  .state('menu.perfilUsuRio', {
+    url: '/perfilUsuario',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/perfilUsuRio.html',
+        controller: 'perfilUsuRioCtrl'
+      }
+    }
+  })
+
+  .state('menu.editarPerfil', {
+    url: '/editarPerfil',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/editarPerfil.html',
+        controller: 'editarPerfilCtrl'
+      }
+    }
+  })
+
+  .state('esqueceuASenha', {
+    url: '/esqueceuSenha',
+    templateUrl: 'templates/esqueceuASenha.html',
+    controller: 'esqueceuASenhaCtrl'
+  })
+
+  .state('menu.meusTPicos', {
+    url: '/meusTopicos',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/meusTPicos.html',
+        controller: 'meusTPicosCtrl'
+      }
+    }
+  })
+
+  .state('menu.tPicosOcultos', {
+    url: '/topicosOcultos',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/tPicosOcultos.html',
+        controller: 'tPicosOcultosCtrl'
+      }
+    }
+  })
+
+  .state('menu.meusComentRios', {
+    url: '/meusComentarios',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/meusComentRios.html',
+        controller: 'meusComentRiosCtrl'
+      }
+    }
+  })
+
+  .state('menu.comentRiosOcultos', {
+    url: '/comentariosOcultos',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/comentRiosOcultos.html',
+        controller: 'comentRiosOcultosCtrl'
+      }
+    }
+  })
+
+  .state('menu.meusComentRios2', {
+    url: '/meusTopicosComentario',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/meusComentRios2.html',
+        controller: 'meusComentRios2Ctrl'
+      }
+    }
+  })
+
+  .state('menu.comentRiosOcultos2', {
+    url: '/topicosComentariosOcultos',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/comentRiosOcultos2.html',
+        controller: 'comentRiosOcultos2Ctrl'
+      }
+    }
+  })
+
+  .state('menu.editarTPico', {
+    url: '/editarTopico?id',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/editarTPico.html',
+        controller: 'editarTPicoCtrl'
+      }
+    }
+  })
+
+  .state('menu.editarComentRio', {
+    url: '/editarComentario',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/editarComentRio.html',
+        controller: 'editarComentRioCtrl'
+      }
+    }
+  })
+
+  .state('menu.cadastrarJogo', {
+    url: '/cadastrarJogo',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/cadastrarJogo.html',
+        controller: 'cadastrarJogoCtrl'
+      }
+    }
+  })
+
+  .state('jogo', {
+    url: '/jogoRating',
+    templateUrl: 'templates/jogo.html',
+    controller: 'jogoCtrl'
+  })
+
+  .state('jogo2', {
+    url: '/jogo',
+    templateUrl: 'templates/jogo2.html',
+    controller: 'jogo2Ctrl'
+  })
+
+  .state('jogo3', {
+    url: '/jogoForum',
+    templateUrl: 'templates/jogo3.html',
+    controller: 'jogo3Ctrl'
+  })
+
+  .state('menu.configuraEs', {
+    url: '/configuracoes',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/configuraEs.html',
+        controller: 'configuraEsCtrl'
+      }
+    }
+  })
+
+  .state('menu.tempoDeFechamento', {
+    url: '/fechamentoTopico',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/tempoDeFechamento.html',
+        controller: 'tempoDeFechamentoCtrl'
+      }
+    }
+  })
+
+  .state('menu.calculoDeRate', {
+    url: '/calculoRate',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/calculoDeRate.html',
+        controller: 'calculoDeRateCtrl'
+      }
+    }
+  })
+
+  .state('menu.relatRios', {
+    url: '/relatorios',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/relatRios.html',
+        controller: 'relatRiosCtrl'
+      }
+    }
+  })
+
+  .state('tPico', {
+    url: '/topico',
+    templateUrl: 'templates/tPico.html',
+    controller: 'tPicoCtrl'
+  })
+
+$urlRouterProvider.otherwise('/side-menu/home')
+
+  
+
+});
---
+++ GameRating/www/js/controllers.js
@@ -0,0 +1,251 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+	console.log("Carreguei a parada!")
+	
+	$scope.search = function(){		
+		console.log("Chamei o Botão!!")
+		console.log($scope.search.value)
+	}
+
+}])
+   
+.controller('buscarUsuRioCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+      
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cadastroCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('perfilCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+	
+	console.log("Carreguei a parada!")
+	$scope.perfilMock = {nome:"leandro",email:"leandro.Malicheski@gmail.com",usuario:"leandroMalicheski",resposta:"Bob",imagePath:"teste"}
+	console.log($scope.perfilMock)
+	$scope.disable = function(){
+		console.log("Vou Desabilitar Você!!")
+	}
+	
+	
+}])
+   
+.controller('perfilUsuRioCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+	
+
+}])
+   
+.controller('editarPerfilCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+	console.log("Carreguei a parada!")
+	$scope.perfilAtualMock = {nome:"leandro",email:"leandro.Malicheski@gmail.com",usuario:"leandroMalicheski",resposta:"Bob",imagePath:"teste",senha:"123",senhaI:"123"}
+	console.log($scope.perfilAtualMock)
+	
+	$scope.salvar = function(perfil){
+		console.log("vou Salvar esse cara!")
+		console.log($scope.perfil)
+	}
+
+}])
+   
+.controller('esqueceuASenhaCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('meusTPicosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+	$scope.topicosMock = [{titulo:"Topico1",checked:true,id:1},{titulo:"Topico2",checked:true,id:2},{titulo:"Topico3",checked:true,id:3}]
+	console.log("Carreguei a parada!")
+	console.log($scope.topicosMock)
+	
+	$scope.topicoSelecionado = function(topico){
+		console.log("selecionei esse cara:")
+		console.log(topico)
+	}
+
+}])
+   
+.controller('tPicosOcultosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('meusComentRiosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('comentRiosOcultosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('meusComentRios2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('comentRiosOcultos2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('editarTPicoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+	var id = $stateParams.id
+	$scope.topicoMock = {id:1,titulo:"Titulo do Topico",corpo:"Corpo do tópico"}
+	
+	console.log("cheguei e estou com id abaixo:")
+	console.log($stateParams.id)
+	
+	$scope.fechar = function(){
+		console.log("Vou fechar o Tópico")
+	}
+	$scope.addLink = function(topicoNovo){
+		console.log("Vou adicionar um link")
+		console.log(topicoNovo)
+	}
+	$scope.remover = function(){
+		console.log("Vou remover o Tópico")
+	}
+	$scope.adicionarImagem = function(topicoNovo){
+		console.log("Vou adicionar Imagem o Tópico")
+		console.log(topicoNovo)
+	}
+	$scope.salvar = function(topicoNovo){
+		console.log("Vou salvar o Tópico")
+		console.log(topicoNovo)
+	}
+
+}])
+   
+.controller('editarComentRioCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cadastrarJogoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('jogoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('jogo2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('jogo3Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('configuraEsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('tempoDeFechamentoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('calculoDeRateCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('relatRiosCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('tPicoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
